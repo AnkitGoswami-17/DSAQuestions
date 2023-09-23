e345cd99ed16cb9d48583659d59e5d4d1a61a5b4
Ankit Goswami <goswamiankito52@gmail.com>
2023-09-23 10:44:13 +0530

    new

---
+++ ArrayQustions/ShuffleArray.java
@@ -0,0 +1,16 @@
+package ArrayQustions;
+
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        for(int i=n;i<2*n;i++){
+            nums[i] = nums[i]<<10;
+            nums[i] |= nums[i-n];
+        }
+        int input=n;
+        for(int i=0;i<2*n;i+=2){
+            nums[i]=nums[input] & 1023;
+            nums[i+1]=nums[input++]>>10;
+        }
+        return nums;
+    }
+}
---
+++ Matrix/RichestCustomerWealth.java
@@ -0,0 +1,30 @@
+package Matrix;
+
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count=0;
+        for(int num:nums){
+            if(even(num)){
+                count++;
+            }
+        }
+        return count;
+    }
+     int digits(int num){
+        int count=0;
+
+        while(num>0){
+            count++;
+            num=num/10;
+        }
+        return count;
+    }
+     boolean even(int num){
+        int numberOfDigits=digits(num);
+        if(numberOfDigits %2==0){
+            return true;
+        }
+        return false;
+    }
+    
+}
